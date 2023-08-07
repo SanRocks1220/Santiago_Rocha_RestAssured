7f5abfa3cdfc1a88305413fc376fb7364cd22fdd
Esteban Bosques <estbosqu@publicisgroupe.net>
2023-08-06 22:45:59 -0600

    basic test

--- demo/src/test/java/AppTest.java
+++ demo/src/test/java/AppTest.java
@@ -1,24 +1,23 @@
-
-
-
 import org.junit.Test;
+
+import config.TestConfig;
+
 import static io.restassured.RestAssured.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest extends TestConfig {
     /**
      * Rigorous Test :-)
      */
     @Test
     public void myFirstTest() {
         given()
-        .log().all()
+            .log().all()
         .when()
-        .get("https://videogamedb.uk/api/videogame")
+            .get("quotes")
         .then()
-        .log().all();
+            .log().all();
     }
 }
