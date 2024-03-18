06ca263d2809e0e747fe2550b23cf16f7c242b28
Santiago Rocha <sanrocha@publicisgroupe.net>
2024-03-18 16:49:18 -0500

    Rest Assured Checkpoint completed

--- demo/src/test/java/AppTest.java
+++ demo/src/test/java/AppTest.java
@@ -1,8 +1,11 @@
 import org.junit.Test;
 
 import config.TestConfig;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 /**
  * Unit test for simple App.
@@ -12,14 +15,83 @@ public class AppTest extends TestConfig {
      * Rigorous Test :-)
      */
     @Test
-    public void myFirstTest() {
+    public void getRequestTest() {
+        String TODOS_ENDPOINT = "{todoId}";
+
         given()
-            .log().all()
+            .pathParam("todoId", "3")
         .when()
-            .get("quotes")
-            // .get("https://dummyjson.com/quotes") // If we didn't have a Base/Config class
-            // we would make the request like this
+            .get(TODOS_ENDPOINT)
+            // .get("https://dummyjson.com/todos/3")
         .then()
-            .log().all();
+            .statusCode(200)
+            .body("id", equalTo(3))
+            .body("todo", startsWith("Watch"))
+            .body("completed", equalTo(false))
+            .body("userId", is(4))
+            .body(matchesJsonSchemaInClasspath("schema.json"));
+    }
+
+    @Test
+    public void postRequestTest() {
+        String TODOS_ENDPOINT = "{todoId}";
+
+        String todoJson = "{"+
+            "\"todo\": \"Build a Lego Set\"," +
+            "\"completed\": true," +
+            "\"userId\": 15" +
+            "}";
+
+        given()
+            .pathParam("todoId", "add")
+            .contentType("application/json")
+            .body(todoJson)
+        .when()
+            .post(TODOS_ENDPOINT)
+            // .post("https://dummyjson.com/todos/add")
+        .then()
+            .statusCode(200)
+            .body("id", equalTo(151))
+            .body("todo", equalTo("Build a Lego Set"))
+            .body("completed", equalTo(true))
+            .body("userId", equalTo(15));
+    }
+
+    @Test
+    public void putRequestTest() {
+        String TODOS_ENDPOINT = "{todoId}";
+
+        String todoJson = "{"+
+            "\"todo\": \"Watch a horror movie\"," +
+            "\"completed\": true," +
+            "\"userId\": 4" +
+            "}";
+
+        given()
+            .pathParam("todoId", "3")
+            .contentType("application/json")
+            .body(todoJson)
+        .when()
+            .put(TODOS_ENDPOINT)
+            // .put("https://dummyjson.com/todos/3")
+        .then()
+            .statusCode(200)
+            .body("id", equalTo(3))
+            .body("todo", startsWith("Watch a horror"))
+            .body("completed", equalTo(true))
+            .body("userId", equalTo(4));
+    }
+
+    @Test
+    public void deleteRequestTest() {
+        String TODOS_ENDPOINT = "{todoId}";
+
+        given()
+            .pathParam("todoId", 3)
+        .when()
+            .delete(TODOS_ENDPOINT)
+            // .put("https://dummyjson.com/todos/3")
+        .then()
+            .statusCode(200);
     }
 }
--- demo/src/test/java/config/TestConfig.java
+++ demo/src/test/java/config/TestConfig.java
@@ -9,10 +9,11 @@ public class TestConfig {
   public static void setup() {
     RestAssured.baseURI = "https://dummyjson.com/"; // This is the base URI
     
-    // RestAssured.basePath = "v2/"; // This is the path (i.e. whats after the base
-        // URI). For this fake api it won't be necessary
+    RestAssured.basePath = "todos/"; // This is the path (i.e. whats after the base
+        // URI).
 
     // RestAssured.port = 443; // For this example it won't be necessary, but just
         // so you know you can set the port
+
   }
 }
